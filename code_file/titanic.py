338611a87597f3eb3c05d3a82f58d95b007dda29
1di0t <jeonjerry0709@gmail.com>
2024-10-31 12:36:58 +0900

    feat: fill NaN to Unknown

--- code_file/titanic.py
+++ code_file/titanic.py
@@ -4,11 +4,10 @@ import pandas as pd
 
 titanic = sns.load_dataset('titanic')
 
-# print(titanic.describe())
-# print(titanic.info())
-# print(titanic.head(10))
-# print(titanic.tail(10))
-# print(len(titanic))
-# print(titanic.shape)
+print(f"기본 버전: {titanic['deck']}")
+titanic['deck'] = titanic['deck'].cat.add_categories('Unknown')
+print(f"카테고리 추가 버전: {titanic['deck']}")
+titanic['deck'].fillna('Unknown',inplace=True)
+print(f"언노운 재배치 버전: {titanic['deck']}")
 
-print(titanic['adult_male'])
+print(titanic.info())
