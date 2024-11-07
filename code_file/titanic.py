610d4cafa0e33fbf11173e0796cebc8a818ef08f
1di0t <jeonjerry0709@gmail.com>
2024-11-07 10:41:44 +0900

    feat: visualization survived count

--- code_file/titanic.py
+++ code_file/titanic.py
@@ -1,13 +1,19 @@
 import seaborn as sns
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 titanic = sns.load_dataset('titanic')
-
-print(f"기본 버전: {titanic['deck']}")
+#add category Unkown
 titanic['deck'] = titanic['deck'].cat.add_categories('Unknown')
-print(f"카테고리 추가 버전: {titanic['deck']}")
+#fill NaN with Unknown
 titanic['deck'].fillna('Unknown',inplace=True)
-print(f"언노운 재배치 버전: {titanic['deck']}")
 
-print(titanic.info())
+print(titanic["survived"].value_counts())
+
+#visualization survived count
+sns.countplot(data=titanic,x='survived')
+plt.title("Survied (0 = No, 1 = Yes)")
+plt.xlabel("Survied")
+plt.ylabel("Count")
+plt.show()
