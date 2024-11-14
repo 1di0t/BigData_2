2668b45d7bf58db4e19ee3dca5d685f141001d99
1di0t <jeonjerry0709@gmail.com>
2024-11-14 12:07:29 +0900

    refactor: Logistic Regression version

--- code_file/titanic.py
+++ code_file/titanic.py
@@ -1,6 +1,6 @@
 import seaborn as sns # based on pandas 
 import matplotlib.pyplot as plt
-from sklearn.neighbors import KNeighborsRegressor
+from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 
 #load titanic dataset
@@ -15,20 +15,21 @@ titanic['age'].fillna(titanic['age'].median(),inplace=True)
 #print(titanic.info())
 
 X = titanic[['age']]
-y = titanic[['survived']]
+y = titanic['survived']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 #model construction
-model = KNeighborsRegressor(n_neighbors=5)
+model = LogisticRegression()
 
 #model training
 model.fit(X_train,y_train)
 
 plt.figure(figsize=(10,6))
 plt.scatter(X_test,y_test,color='orange',label='Real')
-plt.scatter(X_test,model.predict(X_test),color='green',label='Predicted')
-plt.title('Linear Regression Model')
+plt.scatter(X_test,model.predict(X_test),color='green',label='Predicted',alpha=0.3)
+plt.title('Logistic Regression Model')
 plt.xlabel('Age')
 plt.ylabel('Survived')
+plt.legend()
 plt.show()
