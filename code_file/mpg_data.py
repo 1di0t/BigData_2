eaa397332bdf422c0cb32cb505227c967a213d2a
1di0t <jeonjerry0709@gmail.com>
2024-11-21 11:29:55 +0900

    Feat: preprocessing data

--- code_file/mpg_data.py
+++ code_file/mpg_data.py
@@ -1,10 +1,30 @@
 # Mile per gallon data
 
+import pandas as pd
 import seaborn as sns # based on pandas 
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 
+#load mpg dataset
 mpg = sns.load_dataset('mpg')
 
+
+#drop rows with NaN values
 mpg.dropna(axis=0,inplace=True)
-print(mpg['origin'].value_counts())
+#drop name column
+mpg.drop(columns=['name'],inplace=True)
+
+
+#print(mpg['origin'].value_counts())
+
+
+#one hot encoding for origin column
+mpg= pd.get_dummies(mpg,columns=['origin'],drop_first=True)
+
+#print(mpg.head(20))
+
+
+#split data
+X = mpg.drop(columns=['mpg'],axis=1)
+y = mpg['mpg']
+
