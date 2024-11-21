97281c0ffb3bd10c0cc2866efffa7abb85c859d2
1di0t <jeonjerry0709@gmail.com>
2024-11-21 11:54:51 +0900

    Feat: fit model and calculate R2_score,MSE

--- code_file/mpg_data.py
+++ code_file/mpg_data.py
@@ -4,7 +4,8 @@ import pandas as pd
 import seaborn as sns # based on pandas 
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error,r2_score
 #load mpg dataset
 mpg = sns.load_dataset('mpg')
 
@@ -23,8 +24,37 @@ mpg= pd.get_dummies(mpg,columns=['origin'],drop_first=True)
 
 #print(mpg.head(20))
 
-
 #split data
 X = mpg.drop(columns=['mpg'],axis=1)
 y = mpg['mpg']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=45)
+#score increase and mse decrease with random_state=45
+
+#model construction and training
+model = LinearRegression()
+model.fit(X_train,y_train)
+
+#model prediction
+y_pred = model.predict(X_test)
+
+#model evaluation
+mse = mean_squared_error(y_test,y_pred)
+r2 = r2_score(y_test,y_pred)
+
+print("===============================================")
+print(f"R2_Score Error : {r2}")
+print("===============================================")
+print(f"Mean Squared Error : {mse}")
+print("===============================================")
+
+
+#visualization
+plt.figure(figsize=(10,6))
+plt.scatter(y_test,y_pred,color='orange',alpha=0.7)
+plt.plot([y.min(),y.max()],[y.min(),y.max()],color='blue')
+plt.title('Linear Regression Model')
+plt.xlabel('Real')
+plt.ylabel('Predicted')
+plt.show()
 
